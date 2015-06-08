d3b20b517d910fe523ae094115e7d793b83283dd
iKoznov <tirastriel@gmail.com>
2015-06-08 10:27:49 +0300

    read rgb colors

--- main.cpp
+++ main.cpp
@@ -23,7 +23,8 @@ int main (int argc, char *argv[])
   ILuint img_id = ilGenImage(); ilBindImage(img_id);
   BOOST_LOG_TRIVIAL(trace) << boost::format("il img id : %s") %img_id;
   
-  if ( ilLoadImage( (getBasePath()+"icons.png").c_str() ) ) {
+  std::string img_name = "lowres.png";
+  if ( ilLoadImage( (getBasePath()+img_name).c_str() ) ) {
     BOOST_LOG_TRIVIAL(info) << "img loaded";
   }
   else {
@@ -44,7 +45,21 @@ int main (int argc, char *argv[])
   BOOST_LOG_TRIVIAL(trace) << boost::format("  type: %#06x") %img_info.Type;
   BOOST_LOG_TRIVIAL(trace) << boost::format("  origin: %#06x") %img_info.Origin;
   
-  std::string output_fname = getAppDataPath() + "recolor/recolor.png";
+  std::vector< std::vector<glm::vec4> > colors(img_info.Height, std::vector<glm::vec4>(img_info.Width));
+  for (ILuint j=0; j<img_info.Height; ++j) {
+    for (ILuint i=0; i<img_info.Width; ++i) {
+      ILuint offset = (j*img_info.Width+i)*img_info.Bpp;
+      colors[j][i] = glm::vec4(img_info.Data[offset+0]/256.0f,
+                               img_info.Data[offset+1]/256.0f,
+                               img_info.Data[offset+2]/256.0f,
+                               img_info.Data[offset+3]/256.0f);
+      BOOST_LOG_TRIVIAL(trace) << boost::format("%s[%s:%s] %s") %(unsigned)offset %j %i %glm::to_string( colors[j][i] );
+      img_info.Data[offset+0] = 255;
+    }
+  }
+  
+  std::string output_fname = getAppDataPath() + "recolor/" + img_name;
+  std::remove( output_fname.c_str() );
   if ( ilSaveImage(output_fname.c_str()) ) {
     BOOST_LOG_TRIVIAL(info) << "img saved : " << output_fname;
   }
