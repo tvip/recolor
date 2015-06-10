4b99c8e1eed55196eb6e04482ba133e487b8041a
iKoznov <tirastriel@gmail.com>
2015-06-10 21:04:25 +0300

    tool skelet

---
+++ tool/recolor-tool.cpp
@@ -0,0 +1,17 @@
+#include "recolor.hpp"
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#include <IL/il.h>
+#include <IL/ilu.h>
+
+#include <iostream>
+#include <fstream>
+
+
+
+int main (int argc, char *argv[])
+{
+  return 0;
+}
