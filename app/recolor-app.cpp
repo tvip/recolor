c6bdba8c985efd3c4c22c72aa037ede7daef530c
iKoznov <tirastriel@gmail.com>
2015-06-10 19:58:51 +0300

    move tests and app into subdirectory

--- app/recolor-app.cpp
+++ app/recolor-app.cpp
@@ -8,23 +8,21 @@
 #include <IL/il.h>
 #include <IL/ilu.h>
 
-#include <boost/format.hpp>
-#include <boost/log/trivial.hpp>
-
+#include <iostream>
 #include <fstream>
 
 void process_image( const ColorTransition &recolor, const std::string &img_name )
 {
   ILuint img_id = ilGenImage(); ilBindImage(img_id);
-  BOOST_LOG_TRIVIAL(trace) << boost::format("il img id : %s") %img_id;
+  std::cout << "il img id : " << img_id << std::endl;
   
   if ( ilLoadImage( (getBasePath()+img_name).c_str() ) ) {
-    BOOST_LOG_TRIVIAL(info) << "img loaded";
+    std::cout << "img loaded" << std::endl;
   }
   else {
     ILenum error;
     while ((error = ilGetError()) != IL_NO_ERROR) {
-      BOOST_LOG_TRIVIAL(error) << "img not loaded : " << iluErrorString( error );
+      std::cerr << "img not loaded : " << iluErrorString( error ) << std::endl;
     }
     return;
   }
@@ -32,12 +30,12 @@ void process_image( const ColorTransition &recolor, const std::string &img_name 
   ILinfo img_info;
   iluGetImageInfo(&img_info);
   
-  BOOST_LOG_TRIVIAL(info) << boost::format("  dimensions : %sx%sx%s ") %img_info.Width %img_info.Height %img_info.Depth;
-  BOOST_LOG_TRIVIAL(info) << boost::format("  bbp: %s") %(unsigned)img_info.Bpp;
-  BOOST_LOG_TRIVIAL(info) << boost::format("  size: %s") %img_info.SizeOfData;
-  BOOST_LOG_TRIVIAL(trace) << boost::format("  format: %#06x") %img_info.Format;
-  BOOST_LOG_TRIVIAL(trace) << boost::format("  type: %#06x") %img_info.Type;
-  BOOST_LOG_TRIVIAL(trace) << boost::format("  origin: %#06x") %img_info.Origin;
+  std::cout << "  dimensions : " << img_info.Width << "x" << img_info.Height << "x" << img_info.Depth << std::endl;
+  std::cout << "  bbp: " << (unsigned)img_info.Bpp << std::endl;
+  std::cout << "  size: " << img_info.SizeOfData << std::endl;
+  std::cout << "  format: " << img_info.Format << std::endl;
+  std::cout << "  type: " << img_info.Type << std::endl;
+  std::cout << "  origin: " << img_info.Origin << std::endl;
   
   ColorTransition::Image orig_img(img_info.Height, std::vector<glm::vec4>(img_info.Width));
   for (ILuint j=0; j<img_info.Height; ++j) {
@@ -47,9 +45,6 @@ void process_image( const ColorTransition &recolor, const std::string &img_name 
                                  img_info.Data[offset+1]/255.0f,
                                  img_info.Data[offset+2]/255.0f,
                                  img_info.Data[offset+3]/255.0f);
-#if 0
-      BOOST_LOG_TRIVIAL(trace) << boost::format("%s[%s:%s] %s") %(unsigned)offset %j %i %glm::to_string( colors[j][i] );
-#endif
     }
   }
   
@@ -62,21 +57,18 @@ void process_image( const ColorTransition &recolor, const std::string &img_name 
       img_info.Data[offset+1] = 255.f * res_img[j][i].g;
       img_info.Data[offset+2] = 255.f * res_img[j][i].b;
       img_info.Data[offset+3] = 255.f * res_img[j][i].a;
-#if 0
-      BOOST_LOG_TRIVIAL(trace) << boost::format("%s[%s:%s] %s") %(unsigned)offset %j %i %glm::to_string( colors[j][i] );
-#endif
     }
   }
   
   std::string output_fname = getAppDataPath() + "recolor/" + img_name;
   std::remove( output_fname.c_str() );
   if ( ilSaveImage(output_fname.c_str()) ) {
-    BOOST_LOG_TRIVIAL(info) << "img saved : " << output_fname;
+    std::cout << "img saved : " << output_fname << std::endl;
   }
   else {
     ILenum error;
     while ((error = ilGetError()) != IL_NO_ERROR) {
-      BOOST_LOG_TRIVIAL(error) << boost::format("img not saved : %s : %s") %output_fname %iluErrorString( error );
+      std::cerr << "img not saved : " << output_fname << " : " << iluErrorString( error ) << std::endl;
     }
   }
   
@@ -88,10 +80,10 @@ int main (int argc, char *argv[])
   ilInit();
   iluInit();
   
-  std::string cmd = "pwd";
-  BOOST_LOG_TRIVIAL(info) << boost::format("%s : %s") %cmd %exec(cmd);
-  BOOST_LOG_TRIVIAL(info) << "app data : " << getAppDataPath();
-  BOOST_LOG_TRIVIAL(info) << "resources : " << getBasePath();
+  std::string pwd = "pwd";
+  std::cout << pwd << " : " << exec(pwd);
+  std::cout << "app data : " << getAppDataPath();
+  std::cout << "resources : " << getBasePath();
   
   std::fstream fTransition( getBasePath() + "colors.txt", std::ios_base::in );
   std::vector<ColorTransition::Transition > transition;
