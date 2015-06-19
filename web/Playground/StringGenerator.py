6dc206aed5d01558ebb8a1c0047f84e85e4e5672
iKoznov <tirastriel@gmail.com>
2015-06-19 12:36:29 +0300

    StringGenerator

--- web/Playground/StringGenerator.py
+++ web/Playground/StringGenerator.py
@@ -6,10 +6,13 @@ import string
 import cherrypy
 
 class StringGenerator(object):
-  cherrypy.expose()
+  @cherrypy.expose
   def index(self):
     return "Hello World!"
 
-  cherrypy.expose()
+  @cherrypy.expose
   def generate(self):
     return ''.join(random.sample(string.hexdigits, 8))
+
+if __name__ == '__main__':
+  cherrypy.quickstart(StringGenerator())
