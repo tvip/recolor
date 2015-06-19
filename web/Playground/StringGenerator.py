48716cc591aa0dc35a515c6354cea63863482f71
iKoznov <tirastriel@gmail.com>
2015-06-19 13:38:34 +0300

    session

--- web/Playground/StringGenerator.py
+++ web/Playground/StringGenerator.py
@@ -20,7 +20,19 @@ class StringGenerator(object):
 
   @cherrypy.expose
   def generate(self, length=8):
-    return ''.join(random.sample(string.hexdigits, int(length)))
+    some_string = ''.join(random.sample(string.hexdigits, int(length)))
+    cherrypy.session['mystring'] = some_string
+    return some_string
+
+  @cherrypy.expose
+  def display(self):
+    return cherrypy.session['mystring']
 
 if __name__ == '__main__':
-  cherrypy.quickstart(StringGenerator())
+  conf = {
+    '/': {
+      'tools.sessions.on': True
+    }
+  }
+
+  cherrypy.quickstart(StringGenerator(), '/', conf)
