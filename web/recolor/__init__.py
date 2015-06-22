5fc1ad86f0ac4113764b72697459715b498d2617
iKoznov <tirastriel@gmail.com>
2015-06-22 20:17:27 +0300

    log recolor output

--- web/recolor/__init__.py
+++ web/recolor/__init__.py
@@ -9,13 +9,19 @@ class Recolor(object):
     orig_color = 'orig'
     res_color = 'green'
 
-    subprocess.call([
+    proc = subprocess.Popen([
       'utils/bin/recolor-tool',
       'test-data/' + res_color + '/matrix.txt',
       '-in', 'test-data/' + orig_color + '/tvip_light/resources',
       '-out', 'test-data/' + res_color + '/tvip_light/resources',
       '-xpath', '//image[@file]', '-xattr', 'file',
       '-xml', 'test-data/' + orig_color + '/tvip_light/resources.xml'
-    ])
+    ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    exitcode = proc.returncode
 
-    return "Done"
+    cherrypy.request.app.log(str(exitcode))
+    cherrypy.request.app.log(str(out))
+    cherrypy.request.app.log(str(err))
+
+    return out
