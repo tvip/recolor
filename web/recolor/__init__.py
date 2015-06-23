9c09bb23f9356ba983abd6f68dc5584a6de7dcd2
iKoznov <tirastriel@gmail.com>
2015-06-23 14:00:23 +0300

    asyc process output example

--- web/recolor/__init__.py
+++ web/recolor/__init__.py
@@ -16,17 +16,28 @@ def monitor(stream, callback):
       stream.write(line)
 
 
+class MonitoredStream:
+  def __init__(self, stream, callback):
+    self.stream = stream
+    self.callback = callback
+    self._read, self._write = os.pipe()
+
+  def fileno(self):
+    return self._write
+
+  def process(self):
+    os.close(self._write)
+    with os.fdopen(self._read) as f:
+      for line in f:
+        self.callback(line)
+        self.stream.write(line)
+
+
 def f(s):
-  print("Write", s)
-
-
-with monitor(sys.stdout, f) as stream:
-  p = subprocess.Popen(["ls"], stdout=stream)
-  p.communicate()
+  print("Write")
 
 
 class Recolor(object):
-
   def recolor_tool_output(self, string):
     cherrypy.request.app.log(string)
 
@@ -35,9 +46,22 @@ class Recolor(object):
     orig_color = 'orig'
     res_color = 'green'
 
+    stream = MonitoredStream(sys.stdout, f)
+
+    proc = subprocess.Popen([
+        'utils/bin/recolor-tool',
+        'test-data/' + res_color + '/matrix.txt',
+        '-in', 'test-data/' + orig_color + '/tvip_light/resources',
+        '-out', 'test-data/' + res_color + '/tvip_light/resources',
+        '-xpath', '//image[@file]', '-xattr', 'file',
+        '-xml', 'test-data/' + orig_color + '/tvip_light/resources.xml'
+      ], stdout=stream)
+    proc.terminate()
+
     with monitor(sys.stdout, self.recolor_tool_output) as output_log, monitor(sys.stderr, self.recolor_tool_output) as error_log:
       print(type(output_log), type(error_log))
 
+      '''
       proc = subprocess.Popen([
         'utils/bin/recolor-tool',
         'test-data/' + res_color + '/matrix.txt',
@@ -50,5 +74,6 @@ class Recolor(object):
       exitcode = proc.returncode
 
       cherrypy.request.app.log(str(exitcode))
+      '''
 
     return 'Done'
