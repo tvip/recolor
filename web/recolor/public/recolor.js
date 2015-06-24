1bd57bd4010dfffef00965ba437a6963f49440ab
iKoznov <tirastriel@gmail.com>
2015-06-24 13:58:15 +0300

    JS some more logging helpers

--- web/recolor/public/recolor.js
+++ web/recolor/public/recolor.js
@@ -1,28 +1,29 @@
+/// Выводит в консоль все проперти переменной
+/// eval( printAllKeyValues( 'var_name' ) )
+function printAllKeyValues(name) {
+  return "for (var key in " + name + ") console.log('" + name + ".' + key + ' = ' + " + name + "[key])"
+}
+
 function recolor() {
   console.log('welcome to Recolor')
 
   if (!!window.EventSource) {
+    eval( printAllKeyValues( 'EventSource' ) )
+
     var source = new EventSource('http://127.0.0.1:9090/recolor/thing')
-/*
+    eval( printAllKeyValues( 'source' ) )
+
     source.addEventListener('message', function(e) {
-      console.log('RECOLOR_TOOL: LOG:' + e.data);
+      console.log('STATE:' + source.readyState + ' EVENT: ' + e.event + ' MESSAGE:' + e.data)
     }, false)
-*/
-    source.onmessage = function(e) {
-      console.log('RECOLOR_TOOL: LOG:' + e.data);
-    }
 
     source.addEventListener('open', function(e) {
-      console.log('RECOLOR_TOOL: connection established: ' + e.message)
+      console.log('STATE:' + source.readyState + ' EVENT: ' + e.event + ' OPEN: ' + e.data)
     }, false)
 
     source.addEventListener('error', function(e) {
-      if (e.readyState == EventSource.CLOSED) {
-        console.log('RECOLOR_TOOL: connection closed: ' + e.message)
-      }
-      else {
-        console.log('RECOLOR_TOOL: connection error: ' + e.message)
-      }
+      console.log('STATE:' + source.readyState + ' EVENT: ' + e.event + ' ERROR: ' + e.data)
+      source.close()
     }, false)
 
   } else {
--- web/server.py
+++ web/server.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
   cherrypy.config.update('server.conf')
   cherrypy.tree.mount(myapp.Root(), '/myapp', 'myapp.conf')
   cherrypy.tree.mount(band.Band(), '/band')
-  cherrypy.tree.mount(recolor.Recolor(), '/recolor', 'recolor.conf')
+  cherrypy.tree.mount(recolor.Recolor(), '/recolor', 'recolor/root.conf')
   cherrypy.engine.start()
   cherrypy.engine.block()
