e9367c325ebbc7037f432c5b245d72e1a9133047
iKoznov <tirastriel@gmail.com>
2015-06-24 17:58:05 +0300

    make event queue itarable

--- web/playground.py
+++ web/playground.py
@@ -10,28 +10,24 @@ class T(threading.Thread):
 
   def run(self):
     for i in range(3):
-      time.sleep(1)
+      # time.sleep(1)
       # print('post T' + str(i))
       self.eventQueue.put('T' + str(i))
-      time.sleep(0.3)
+      # time.sleep(0.3)
       # print('proc T' + str(i))
 
     self.eventQueue.put(None)
 
+  def __iter__(self):
+    while True:
+      message = self.eventQueue.get(block=True)
+      if message:
+        # time.sleep(2)
+        yield message
+      else:
+        break
 
-def gen():
-  t = T()
-  t.start()
-
-  while True:
-    message = t.eventQueue.get(block=True)
-
-    if message:
-      # time.sleep(2)
-      yield message
-    else:
-      break
-
-
-for s in gen():
+t = T()
+t.start()
+for s in t:
   print(s)
