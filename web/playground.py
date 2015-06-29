09cf30f38bd955e8d76d28f5eaefe2ff713990c1
iKoznov <tirastriel@gmail.com>
2015-06-29 18:53:42 +0300

    don't hardcode web server config paths

--- web/playground.py
+++ web/playground.py
@@ -1,4 +1,5 @@
 import threading
+import multiprocessing
 import queue
 import time
 
@@ -27,7 +28,58 @@ class T(threading.Thread):
       else:
         break
 
+
+'''
 t = T()
 t.start()
 for s in t:
   print(s)
+'''
+
+'''
+def count(n):
+  while n > 0:
+    n -= 1
+
+start_time = time.time()
+count(10000000)
+# print("--- {} seconds ---".format(time.time() - start_time))
+count(10000000)
+print("--- {} seconds ---".format(time.time() - start_time))
+
+start_time = time.time()
+t1 = threading.Thread(target=count, args=(10000000,))
+t1.start()
+t2 = threading.Thread(target=count, args=(10000000,))
+t2.start()
+t1.join()
+t2.join()
+print("--- {} seconds ---".format(time.time() - start_time))
+
+start_time = time.time()
+p1 = multiprocessing.Process(target=count, args=(10000000,))
+p1.start()
+p2 = multiprocessing.Process(target=count, args=(10000000,))
+p2.start()
+p1.join()
+p2.join()
+print("--- {} seconds ---".format(time.time() - start_time))
+'''
+
+
+def f(q):
+  q.put('X ' * 10000)
+
+
+if __name__ == '__main__':
+  start_time = time.time()
+
+  queue = multiprocessing.Queue()
+  p = multiprocessing.Process(target=f, args=(queue,))
+  p.start()
+  # p.join()  # this deadlocks
+  obj = queue.get()
+  
+  print(obj)
+
+  print("--- {} seconds ---".format(time.time() - start_time))
--- web/server.py
+++ web/server.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python3
 
 import cherrypy
+import jinja2
+
+import os
 
 import band
 import myapp
 import recolor
 
 
+def setup_config(template_fname, result_fname, env):
+  with open(template_fname) as config:
+    template = jinja2.Template(config.read())
+    res_config = template.render(env)
+
+  with open(result_fname, 'w') as config:
+    config.write(res_config)
+
+  return result_fname
+
+
 if __name__ == '__main__':
   cherrypy.config.update('server.conf')
-  cherrypy.tree.mount(myapp.Root(), '/myapp', 'myapp.conf')
   cherrypy.tree.mount(band.Band(), '/band')
-  cherrypy.tree.mount(recolor.Recolor(), '/recolor', 'recolor/root.conf')
+
+  cherrypy.tree.mount(
+    myapp.Root(), '/myapp',
+    setup_config(
+      template_fname='myapp.conf',
+      result_fname='var/myapp.conf',
+      env={
+        'staticdir': os.path.abspath(os.getcwd())
+      }
+    ))
+
+  cherrypy.tree.mount(
+    recolor.Recolor(), '/recolor',
+    setup_config(
+      template_fname='recolor/root.conf',
+      result_fname='var/recolor.conf',
+      env={
+        'staticdir': os.path.join(os.path.abspath(os.getcwd()), 'recolor')
+      }
+    ))
+
   cherrypy.engine.start()
   cherrypy.engine.block()
