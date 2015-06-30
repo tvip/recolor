39f4088805eb864c17338889a7677b137e7ed00d
iKoznov <tirastriel@gmail.com>
2015-06-30 18:09:17 +0300

    relative import in jinja example
    --HG--
    branch : parallel

--- web/jinja2_templating/jinja2tool.py
+++ web/jinja2_templating/jinja2tool.py
@@ -6,9 +6,10 @@ __all__ = ['Jinja2Tool']
 
 class Jinja2Tool(cherrypy.Tool):
   def __init__(self):
-    cherrypy.Tool.__init__(self, 'before_finalize',
-                           self._render,
-                           priority=10)
+    cherrypy.Tool.__init__(
+      self, 'before_finalize', self._render,
+      priority=10
+    )
 
   def _render(self, template=None, debug=False):
     """
@@ -18,9 +19,9 @@ class Jinja2Tool(cherrypy.Tool):
     whatever dictionary the page handler returned.
     """
 
-    #print(dir(cherrypy.response))
-    #print(type(cherrypy.response.status))
-    #print(cherrypy.response.status)
+    # print(dir(cherrypy.response))
+    # print(type(cherrypy.response.status))
+    # print(cherrypy.response.status)
 
     if cherrypy.response.status is not None and cherrypy.response.status > 399:
       return
@@ -29,12 +30,12 @@ class Jinja2Tool(cherrypy.Tool):
     data = cherrypy.response.body or {}
     template = cherrypy.engine.publish("lookup-template", template).pop()
 
-    #print('Template', template)
-    #print('Response', cherrypy.response)
-    #print('Body', cherrypy.response.body)
-    #print('Body', type(cherrypy.response.body))
+    # print('Template', template)
+    # print('Response', cherrypy.response)
+    # print('Body', cherrypy.response.body)
+    # print('Body', type(cherrypy.response.body))
 
-    #print('Render', template.render(data))
+    # print('Render', template.render(data))
 
     if template and isinstance(data, dict):
       cherrypy.response.body = template.render(data).encode()
