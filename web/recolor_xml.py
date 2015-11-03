81f5c8d02d4155ed8cdcd2cd559a818cd6612cc6
ik <ik@tvip.ru>
2015-11-03 21:19:05 +0300

    color parsing

--- web/recolor_xml.py
+++ web/recolor_xml.py
@@ -5,10 +5,29 @@ import re
 import xml.etree.ElementTree as ET
 
 
+def color_from_string(string):
+    match_hex_color = re.match('^#(([0-9a-f]){6}|([0-9a-f]){8})$', string, flags=re.IGNORECASE)
+    if match_hex_color:
+        hex_string = match_hex_color.groups()[0]
+        result = []
+        i = 0
+        while True:
+            component = hex_string[i:i+2]
+            if not component: break
+            result.append(int(component, 16))
+            i += 2
+        return result
+
+
 def recolor_node(node):
-    # print(node.tag, node.attrib)
-    if node.tag == 'color':
-        print('color', node.text)
+    for key, value in node.attrib.items():
+        color = color_from_string(value)
+        if color:
+            print('ATTR', color)
+    if node.text:
+        color = color_from_string(node.text)
+        if color:
+            print('TEXT', color)
     for child in node:
         recolor_node(child)
 
@@ -25,11 +44,3 @@ if __name__ == '__main__':
         for file in files:
             if '.xml' == os.path.splitext(file)[-1]:
                 recolor_xml(os.path.join(root, file))
-
-    match_hex_color = re.match('^#(([0-9a-f]){6}|([0-9a-f]){8})$', '#11224467')
-    if match_hex_color:
-        hex_string = match_hex_color.groups()[0]
-        number = int(hex_string, 16)
-
-        print('match', int(hex_string, 16))
-        pass
