22539d306e0e504dfff5066333d966653882500d
ik <ik@tvip.ru>
2015-11-25 21:57:44 +0300

    yuv

---
+++ web/yuv.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+w_r = 0.299
+w_b = 0.114
+w_g = 1 - w_r - w_b
+
+u_max = 0.436
+v_max = 0.615
+
+
+def yuv2rgb(y, u, v):
+    return (
+        y + v * (1 - w_r) / v_max,
+        y - u * w_b * (1 - w_b) / (u_max * w_g) - v * w_r * (1 - w_r) / (v_max * w_g),
+        y + u * (1 - w_b) / u_max
+        # y + 1.13983 * (v - 128),
+        # y - 0.39465 * (u - 128) - 0.58060 * (v - 128),
+        # y + 2.03211 * (u - 128)
+    )
+
+
+def rgb2yuv(r, g, b):
+    y = w_r * r + w_g * g + w_b * b
+    return (
+        y,
+        u_max * ((b - y) / (1 - w_b)),
+        v_max * ((r - y) / (1 - w_r))
+        # -0.14713 * r - 0.28886 * g + 0.436 * b + 128,
+        # 0.615 * r - 0.51499 * g - 0.10001 * b + 128
+    )
+
+
+if __name__ == '__main__':
+    yuv = rgb2yuv(1, 0, 0)
+    rgb = yuv2rgb(1, 0, 0)
+    print(yuv)
+    print(rgb)
+
+
